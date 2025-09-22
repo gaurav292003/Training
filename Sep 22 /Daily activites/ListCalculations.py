da4bbe494615aba1cf690504339629ae9af8d989
gaurav292003 <121452470+gaurav292003@users.noreply.github.com>
2025-09-22 13:40:37 +0530

    Delete Sep 22  directory

--- Sep 22 /Daily activites/ListCalculations.py
+++
@@ -1,7 +0,0 @@
-numbers=[1,2,3,45,6,8]
-
-print(f"The length of list is {len(numbers)}")
-print(f"The sum of list is {sum(numbers)}")
-print(f"The min of list is {min(numbers)}")
-print(f"The max of list is {max(numbers)}")
-print(f"The max of list is {sum(numbers)/len(numbers)}") #Average
--- Sep 22 /Daily activites/hello.py
+++
@@ -1,6 +0,0 @@
-def greet(user):
-    return f"Hello, {user}"
-
-name = input("What is your name? ")
-message= greet(name)
-print(message)
